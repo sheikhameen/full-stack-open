8c32dc4dedb33f3ae6c0da6cb07e4a877431a6e9
Sheikh Ameen <sheikhameen123@gmail.com>
2022-04-08 21:15:05 +0800

    Completed 5.9: Blog list frontend, step9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -125,9 +125,12 @@ const App = () => {
         </Togglable>
       </div>
       <br />
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs} />
-      )}
+      {
+        blogs
+          .sort((a, b) => b.likes - a.likes)
+          .map(blog =>
+            <Blog key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs} />)
+      }
     </div>
   )
 
