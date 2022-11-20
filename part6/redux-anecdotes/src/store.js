e82093df6b372f775e1af8ddecb0ec4fe1429ac4
Sheikh Ameen <sheikhameen123@gmail.com>
2022-11-20 14:25:00 +0800

    Completed 6.9 Better anecdotes, step7

---
+++ part6/redux-anecdotes/src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import anecdoteReducer from './reducers/anecdoteReducer'
+
+const store = configureStore({
+  reducer: {
+    anecdote: anecdoteReducer
+  }
+})
+
+export default store
