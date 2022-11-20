7f4a09158102b4dffa0b02f962cba21b8de6d563
Sheikh Ameen <sheikhameen123@gmail.com>
2022-11-20 14:51:35 +0800

    Completed 6.10 Better anecdotes, step8

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import anecdoteReducer from './reducers/anecdoteReducer'
+import notificationReducer from './reducers/notificationReducer'
 
 const store = configureStore({
   reducer: {
-    anecdote: anecdoteReducer
+    anecdote: anecdoteReducer,
+    notification: notificationReducer
   }
 })
 
