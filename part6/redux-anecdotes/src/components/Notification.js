4e852cce347eb6a0b75031f15d92c7481559430b
Sheikh Ameen <sheikhameen123@gmail.com>
2022-11-23 14:52:28 +0800

    Completed 6.19 anecdotes and connect, step1

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,11 +1,8 @@
 import { useEffect, useState } from 'react'
-// import { useSelector, useDispatch } from 'react-redux'
-import { useSelector } from 'react-redux'
-// import { removeNotification } from '../reducers/notificationReducer'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  // const dispatch = useDispatch()
-  const notification = useSelector(state => state.notification)
+const Notification = (props) => {
+  const notification = props.notification
   const [show, setShow] = useState(false)
 
   useEffect(() => {
@@ -15,33 +12,12 @@ const Notification = () => {
       setShow(false)
     }
   }, [notification])
-  // useEffect(() => {
-  //   if (!notification) return
-
-  //   setShow(true)
-  //   console.log('NEW TIMEOUT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-  //   const timeout = setTimeout(() => {
-  //     setShow(false);
-  //     dispatch(removeNotification())
-  //   }, 5000)
-
-  //   return () => {
-  //     console.log('CLEARING TIMEOUT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-  //     clearTimeout(timeout)
-  //   }
-  // }, [notification, dispatch])
 
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
-
-  // return (
-  //   <div style={style}>
-  //     {notification}
-  //   </div>
-  // )
 
   return show ? (
     <div style={style}>
@@ -50,6 +26,12 @@ const Notification = () => {
   ) : null
 }
 
+const mapStateToProps = state => {
+  return {
+    notification: state.notification
+  }
+}
 
+const ConnectedNotification = connect(mapStateToProps)(Notification)
 
-export default Notification
+export default ConnectedNotification
