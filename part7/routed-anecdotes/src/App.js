8912969e9cb56aafc456d471d12af094feda2451
Sheikh Ameen <sheikhameen123@gmail.com>
2022-11-26 13:50:16 +0800

    Completed 7.5: anecdotes and hooks step2

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -71,9 +71,6 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  // const [content, setContent] = useState('')
-  // const [author, setAuthor] = useState('')
-  // const [info, setInfo] = useState('')
   const content = useField('text')
   const author = useField('text')
   const info = useField('url')
@@ -89,10 +86,17 @@ const CreateNew = (props) => {
     })
     navigate('/')
 
-    props.setNotification(`A new anecdote "${content.value} created!"`)
+    props.setNotification(`A new anecdote "${content.value}" created!`)
     setTimeout(() => {
       props.setNotification('')
     }, 5000);
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
   }
 
   return (
@@ -112,6 +116,7 @@ const CreateNew = (props) => {
           <input name='info' {...info} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
