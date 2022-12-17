c5250ffbd2e3f61bee394e78882623ac18cc1fa1
Sheikh Ameen <sheikhameen123@gmail.com>
2022-12-17 13:05:28 +0800

    Added react-router-dom, wrapped App with BrowserRouter

--- part7/bloglist/frontend/src/index.js
+++ part7/bloglist/frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
+import { BrowserRouter as Router } from "react-router-dom"
 import store from "./store"
 import App from "./App"
 
 const root = createRoot(document.getElementById("root"))
 root.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>
 )
