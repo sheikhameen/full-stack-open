c5b678e7f5120bc01559afed0e2c4624019322a8
Sheikh Ameen <sheikhameen123@gmail.com>
2024-07-30 16:53:15 +0800

    Completed 8.4: Books of an author

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -116,7 +116,7 @@ const typeDefs = `#graphql
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -125,7 +125,14 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books;
+      }
+
+      const filteredBooks = books.filter((book) => book.author === args.author);
+      return filteredBooks;
+    },
     allAuthors: () => authors,
   },
 
