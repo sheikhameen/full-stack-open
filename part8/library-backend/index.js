b9c0d90d1bab37e0d8fc998fb31d5cfae70366fd
Sheikh Ameen <sheikhameen123@gmail.com>
2024-07-30 16:17:12 +0800

    Completed 8.1: The number of books and authors

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -97,15 +97,17 @@ let books = [
   you can remove the placeholder query once your first one has been implemented 
 */
 
-const typeDefs = `
+const typeDefs = `#graphql
   type Query {
-    dummy: Int
+    bookCount: Int!
+    authorCount: Int!
   }
 `;
 
 const resolvers = {
   Query: {
-    dummy: () => 0,
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
   },
 };
 
