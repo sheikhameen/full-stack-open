137a8b1f3db5476795c8486b2b3dee335655bd15
Sheikh Ameen <sheikhameen123@gmail.com>
2024-07-30 16:23:24 +0800

    Completed 8.2: All books

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -98,9 +98,17 @@ let books = [
 */
 
 const typeDefs = `#graphql
+  type Book {
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `;
 
@@ -108,6 +116,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 };
 
