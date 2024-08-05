8a4761aa6def11e69fbfb97cd88b0c56a60ba8cb
Sheikh Ameen <sheikhameen123@gmail.com>
2024-08-05 16:11:33 +0800

    Completed 9.9: Patientor backend, step2

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import cors from "cors";
 const app = express();
+// app.use(cors());
+app.use((cors as (options: cors.CorsOptions) => express.RequestHandler)({}));
 
 app.get("/api/ping", (_req, res) => {
   return res.send("pong");
 });
 
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
