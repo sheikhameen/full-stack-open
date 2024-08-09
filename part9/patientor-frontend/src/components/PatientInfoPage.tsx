2dc6305ff68ba2248dbffd9f85092ace32358ad9
Sheikh Ameen <sheikhameen123@gmail.com>
2024-08-09 12:32:04 +0800

    Completed 9.25: Patientor, step 6

--- part9/patientor-frontend/src/components/PatientInfoPage.tsx
+++ part9/patientor-frontend/src/components/PatientInfoPage.tsx
@@ -1,10 +1,22 @@
 import patientService from "../services/patients";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Diagnosis, Patient } from "../types";
+import {
+  Diagnosis,
+  Entry,
+  HealthCheckEntry,
+  HealthCheckRating,
+  HospitalEntry,
+  OccupationalHealthcareEntry,
+  Patient,
+} from "../types";
 import MaleIcon from "@mui/icons-material/Male";
 import FemaleIcon from "@mui/icons-material/Female";
 import PersonIcon from "@mui/icons-material/Person";
+import MedicalServicesIcon from "@mui/icons-material/MedicalServices";
+import WorkIcon from "@mui/icons-material/Work";
+import { LocalHospital } from "@mui/icons-material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const PatientInfoPage = ({ diagnoses }: { diagnoses: Diagnosis[] }) => {
   const id = useParams().id;
@@ -27,21 +39,130 @@ const PatientInfoPage = ({ diagnoses }: { diagnoses: Diagnosis[] }) => {
       <p>Occupation: {patient.occupation}</p>
       <h3>Entries</h3>
       {patient.entries.map((e) => (
-        <div key={e.id}>
-          <p>
-            {e.date} <em>{e.description}</em>
-          </p>
-          <ul>
-            {e.diagnosisCodes &&
-              e.diagnosisCodes.map((c) => (
-                <li key={c}>
-                  {c}: {diagnoses.find((d) => d.code === c)?.name}
-                </li>
-              ))}
-          </ul>
+        <div
+          key={e.id}
+          style={{
+            border: "2px black solid",
+            padding: 10,
+            marginBottom: 6,
+            borderRadius: 20,
+          }}
+        >
+          <EntryDetails entry={e} diagnoses={diagnoses} />
         </div>
       ))}
     </div>
+  );
+};
+
+const EntryDetails = ({
+  entry,
+  diagnoses,
+}: {
+  entry: Entry;
+  diagnoses: Diagnosis[];
+}) => {
+  switch (entry.type) {
+    case "Hospital":
+      return <HospitalEntryComponent entry={entry} diagnoses={diagnoses} />;
+    case "HealthCheck":
+      return <HealthCheckEntryComponent entry={entry} diagnoses={diagnoses} />;
+    case "OccupationalHealthcare":
+      return (
+        <OccupationalHealthcareEntryComponent
+          entry={entry}
+          diagnoses={diagnoses}
+        />
+      );
+    default:
+      assertNever(entry);
+  }
+};
+
+const HospitalEntryComponent = ({
+  entry,
+  diagnoses,
+}: {
+  entry: HospitalEntry;
+  diagnoses: Diagnosis[];
+}) => {
+  return (
+    <>
+      <p>
+        {entry.date} <LocalHospital />
+      </p>
+      <p>
+        <em>{entry.description}</em>
+      </p>
+      <ul>
+        {entry.diagnosisCodes &&
+          entry.diagnosisCodes.map((c) => (
+            <li key={c}>
+              {c}: {diagnoses.find((d) => d.code === c)?.name}
+            </li>
+          ))}
+      </ul>
+      <p>Diagnosed by {entry.specialist}</p>
+    </>
+  );
+};
+
+const HealthCheckEntryComponent = ({
+  entry,
+  diagnoses,
+}: {
+  entry: HealthCheckEntry;
+  diagnoses: Diagnosis[];
+}) => {
+  return (
+    <>
+      <p>
+        {entry.date} <MedicalServicesIcon />
+      </p>
+      <p>
+        <em>{entry.description}</em>
+      </p>
+      <ul>
+        {entry.diagnosisCodes &&
+          entry.diagnosisCodes.map((c) => (
+            <li key={c}>
+              {c}: {diagnoses.find((d) => d.code === c)?.name}
+            </li>
+          ))}
+      </ul>
+      <div>
+        <HealthIcon health={entry.healthCheckRating} />
+      </div>
+      <p>Diagnosed by {entry.specialist}</p>
+    </>
+  );
+};
+
+const OccupationalHealthcareEntryComponent = ({
+  entry,
+  diagnoses,
+}: {
+  entry: OccupationalHealthcareEntry;
+  diagnoses: Diagnosis[];
+}) => {
+  return (
+    <>
+      <p>
+        {entry.date} <WorkIcon /> {entry.employerName}
+      </p>
+      <p>
+        <em>{entry.description}</em>
+      </p>
+      <ul>
+        {entry.diagnosisCodes &&
+          entry.diagnosisCodes.map((c) => (
+            <li key={c}>
+              {c}: {diagnoses.find((d) => d.code === c)?.name}
+            </li>
+          ))}
+      </ul>
+      <p>Diagnosed by {entry.specialist}</p>
+    </>
   );
 };
 
@@ -54,6 +175,27 @@ const GenderIcon = ({ gender }: { gender: string }) => {
     default:
       return <PersonIcon />;
   }
+};
+
+const HealthIcon = ({ health }: { health: HealthCheckRating }) => {
+  switch (health) {
+    case HealthCheckRating.Healthy:
+      return <FavoriteIcon style={{ fill: "green" }} />;
+    case HealthCheckRating.LowRisk:
+      return <FavoriteIcon style={{ fill: "yellow" }} />;
+    case HealthCheckRating.HighRisk:
+      return <FavoriteIcon style={{ fill: "orange" }} />;
+    case HealthCheckRating.CriticalRisk:
+      return <FavoriteIcon style={{ fill: "red" }} />;
+    default:
+      assertNever(health);
+  }
+};
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
 };
 
 export default PatientInfoPage;
